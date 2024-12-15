4924a8bb5c4531ae2daadb4621199d860658f50f
vtlinh02 <vutuanlinh02122002@gmail.com>
2024-12-15 12:17:46 +0700

    feat: deploy Backend and Database successfully. Now they can connect to each other in container environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,16 +13,17 @@ import { CartModule } from './modules/cart/cart.module';
 import { Wallet } from './entities/Wallet.entity';
 import { WalletModule } from './modules/wallet/wallet.module';
 import { AuthModule } from './modules/auth/auth.module';
+import 'dotenv/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'vutuanlinh2002',
-      database: 'thesis',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       entities: [Shop, Customer, Product, Order, Cart, Wallet],
       synchronize: true,
     }),
